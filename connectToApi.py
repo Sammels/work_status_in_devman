87ee305a30bda49fe66ee56ed846adae9912f344
sammels <sasagete@gmail.com>
2024-02-08 20:50:55 +0300

    feature: add tg bot.

--- connectToApi.py
+++ connectToApi.py
@@ -1,13 +1,15 @@
-import pprint
 from time import sleep
 
 import requests
-from retry import retry
+import telegram
+
 from environs import Env
+from textwrap import dedent
+from retry import retry
 
 
-@retry((requests.exceptions.ReadTimeout, ConnectionError), delay=30)
-def check_api_devman(token):
+
+def check_api_devman(devman_token, params):
     """
     :param token: str
     :return: str
@@ -15,40 +17,51 @@ def check_api_devman(token):
     url = "https://dvmn.org/api/long_polling/"
 
     header = {
-        "Authorization": f"Token {token}"
+        "Authorization": f"Token {devman_token}"
     }
-    response = requests.get(url, headers=header)
+    response = requests.get(url, headers=header, params=params)
     response.raise_for_status()
-    server_response = response.json()
+    return response.json()
 
+
+@retry((requests.exceptions.ReadTimeout, ConnectionError), delay=30)
+def check_reviews(token, params, bot, tg_chat_id):
     while True:
-        if server_response['status'] == 'found':
-            server_timestamp = server_response['last_attempt_timestamp']
-        else:
-            server_timestamp = server_response['timestamp_to_request']
-
-        params = {
-            'timestamp': int(server_timestamp),
-        }
-
-        response = requests.get(url, headers=header, params=params, timeout=90)
-
-        response.raise_for_status()
-        server_response = response.json()
-        pprint.pprint(server_response)
+        reviews = check_api_devman(token, params)
         sleep(30)
+        if reviews['status'] == 'timeout':
+            timestamp_to_request = reviews['timestamp_to_request']
+            params = {
+                'timestamp': timestamp_to_request,
+            }
+        else:
+            current_review = reviews['new_attempts'][0]
+            if current_review['is_negative']:
+                bot.send_message(chat_id=tg_chat_id,
+                                 text=dedent(f"""
+                                 Работа проверена *"{current_review["lesson_title"]}"*\.
+                                 Есть ошибки\.
+                                 [Ссылка на работу]({current_review["lesson_url"]})
+                                 """),
+                                 parse_mode=telegram.ParseMode.MARKDOWN_V2,
+                                 )
+            else:
+                bot.send_message(chat_id=tg_chat_id,
+                                 text=dedent(f"""
+                                 Работа проверена *"{current_review["lesson_title"]}"*\.
+                                 Все окей, делай дальше\.
+                                 """),
+                                 parse_mode=telegram.ParseMode.MARKDOWN_V2,
+                                 )
 
-    return pprint.pprint(params)
-
-
-def main():
-    env = Env()
-    env.read_env()
-
-    token = env("TOKEN")
-
-    works = check_api_devman(token)
 
 
 if __name__ == "__main__":
-    main()
+    env = Env()
+    env.read_env()
+    devman_token = env.str('DEVMAN_TOKEN')
+    tg_bot_token = env('TG_BOT_TOKEN')
+    tg_chat_id = env('TG_CHAT_ID')
+    params = {}
+    bot = telegram.Bot(token=tg_bot_token)
+    check_reviews(devman_token, params, bot, tg_chat_id)
